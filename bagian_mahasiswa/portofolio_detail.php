7a67356a7db29aafb4e7ecf09c0f6d71ff5ce8e2
holyblesty <jesinaaurora@gmail.com>
2026-01-08 22:29:45 +0700

    menambah komentar

--- bagian_mahasiswa/portofolio_detail.php
+++ bagian_mahasiswa/portofolio_detail.php
@@ -1,11 +1,15 @@
-<?php
-/*
-  Nama File   : portofolio_detail.php
-  Deskripsi   : Halaman mahasiswa untuk menambah, mengedit, dan menghapus data portofolio
-  Pembuat    : Vivian Sarah Diva Alisianoi & Jesina Holyblesty Simatupang
-  Tanggal    : 26 Desember 2025
-*/
+<!-- 
+=========================================================
+  Nama File   : aplikasi-pengumuman-akademik-online.html
+  Deskripsi   : Halaman portofolio Projek PBL
+                Sistem Aplikasi Pengumuman Akademik Online
+  Pembuat     : Jesina HolyBlesty Simatupang (3312511017)
+              : Vivian Sarah Diva Alisianoi (3312511018)
+  Tanggal     : 19 Oktober 2025
+=========================================================
+-->
 
+<?php
 session_set_cookie_params(0);
 session_start();
 include "../koneksi.php";
