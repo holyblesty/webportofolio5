1f98bc5b761911110d0ad5582d1c53e110a62f5f
holyblesty <jesinaaurora@gmail.com>
2026-01-08 21:56:40 +0700

    merapikan kode

--- bagian_mahasiswa/portofolio_detail.php
+++ bagian_mahasiswa/portofolio_detail.php
@@ -158,13 +158,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <meta charset="UTF-8">
-    <title><?= $edit ? "Edit" : "Tambah" ?> Portofolio</title>
+<meta charset="UTF-8">
+<title><?= $edit ? "Edit" : "Tambah" ?> Portofolio</title>
 
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-    >
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         body {
