7adeb567f0ec7f1f1ceb811f7bdf3bdd0d4be20b
holyblesty <jesinaaurora@gmail.com>
2026-01-08 22:19:00 +0700

    menambah komentar

--- bagian_dosen/ganti_password_dsn.php
+++ bagian_dosen/ganti_password_dsn.php
@@ -1,11 +1,15 @@
-<?php
-/*
-  Nama File   : ganti_password_dsn.php
-  Deskripsi   : Halaman dosen untuk mengganti password akun
-  Pembuat    : Vivian Sarah Diva Alisianoi & Jesina Holyblesty Simatupang
-  Tanggal    : 26 Desember 2025
-*/
+<!-- 
+=========================================================
+  Nama File   : aplikasi-pengumuman-akademik-online.html
+  Deskripsi   : Halaman portofolio Projek PBL
+                Sistem Aplikasi Pengumuman Akademik Online
+  Pembuat     : Jesina HolyBlesty Simatupang (3312511017)
+              : Vivian Sarah Diva Alisianoi (3312511018)
+  Tanggal     : 19 Oktober 2025
+=========================================================
+-->
 
+<?php
 session_set_cookie_params(0);
 session_start();
 include "../koneksi.php";
