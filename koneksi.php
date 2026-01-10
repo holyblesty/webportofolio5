2042eaf4a8c6f8a72ad7290e7f830fe5bf0462be
holyblesty <jesinaaurora@gmail.com>
2026-01-10 18:26:59 +0700

    merapikan kode

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,15 @@
-<?php
+<!-- 
+=========================================================
+  Nama File   : aplikasi-pengumuman-akademik-online.html
+  Deskripsi   : Halaman portofolio Projek PBL
+                Sistem Aplikasi Pengumuman Akademik Online
+  Pembuat     : Jesina HolyBlesty Simatupang (3312511017)
+              : Vivian Sarah Diva Alisianoi (3312511018)
+  Tanggal     : 19 Oktober 2025
+=========================================================
+-->
 
+<?php
 // koneksi.php
 $host = "localhost";
 $user = "root";
