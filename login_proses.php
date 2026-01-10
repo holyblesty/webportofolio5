6d461b48a9536427339f60a7c18fe0eae8dcb138
holyblesty <jesinaaurora@gmail.com>
2026-01-10 18:29:00 +0700

    merapikan kode

--- login_proses.php
+++ login_proses.php
@@ -1,3 +1,14 @@
+<!-- 
+=========================================================
+  Nama File   : aplikasi-pengumuman-akademik-online.html
+  Deskripsi   : Halaman portofolio Projek PBL
+                Sistem Aplikasi Pengumuman Akademik Online
+  Pembuat     : Jesina HolyBlesty Simatupang (3312511017)
+              : Vivian Sarah Diva Alisianoi (3312511018)
+  Tanggal     : 19 Oktober 2025
+=========================================================
+-->
+
 <?php
 // session cookie hanya hidup selama browser terbuka
 session_set_cookie_params(0);
