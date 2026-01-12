58d5af2b1d473382755112fe409199f69f7cc433
holyblesty <jesinaaurora@gmail.com>
2026-01-12 15:13:22 +0700

    merapikan kode koneksi database

--- koneksi.php
+++ koneksi.php
@@ -1,22 +1,24 @@
-<!-- 
+<?php
+/* 
 =========================================================
-  Nama File   : aplikasi-pengumuman-akademik-online.html
-  Deskripsi   : Halaman portofolio Projek PBL
-                Sistem Aplikasi Pengumuman Akademik Online
+  Nama File   : koneksi.php
+  Deskripsi   : File koneksi database aplikasi Web Portofolio PBL
   Pembuat     : Jesina HolyBlesty Simatupang (3312511017)
               : Vivian Sarah Diva Alisianoi (3312511018)
   Tanggal     : 19 Oktober 2025
 =========================================================
--->
+*/
 
-<?php
-// koneksi.php
+// konfigurasi database
 $host = "localhost";
 $user = "root";
-$pass = "";$db   = "proyek"; // nama database
+$pass = "";
+$db   = "proyek";
 
+// proses koneksi database
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
+// pengecekan koneksi
 if (!$koneksi) {
     die("Gagal koneksi database: " . mysqli_connect_error());
 }
